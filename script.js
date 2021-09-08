8cb297fa5f0a39b32f7f4c7be6f44a752a85b0a4
jair <jair.duda@gmail.com>
2021-09-08 08:03:23 -0300

    criando mobile first e ciando buttons

--- script.js
+++ script.js
@@ -3,14 +3,9 @@ function girarDados(){
     const dadoDois = (min, max) => Math.floor(Math.random() * (max -min) + min)
     const valor1 = dadoUm(1, 7)
     const valor2 = dadoDois(1,7)
-    // console.log(valor1)
-    // console.log(valor2)
     const result = valor1 + valor2
 
-    
     return result
-
-
 }
 
 let result = girarDados()
@@ -20,14 +15,9 @@ function girarDadosMil(){
     const dadoDois = (min, max) => Math.floor(Math.random() * (max -min) + min)
     const valor1 = dadoUm(1, 7)
     const valor2 = dadoDois(1,7)
-    // console.log(valor1)
-    // console.log(valor2)
     const result = valor1 + valor2
 
-    
     return result
-
-
 }
 
 
@@ -37,10 +27,7 @@ const buttonGira = document.createElement("button")
     buttonGira.addEventListener("click", () => {
         let resultado1 = girarDados()
         dado.innerText = resultado1;
-
     })
-
-
 
 buttonGira.classList.add("giraDados");
 
@@ -50,9 +37,7 @@ document.body.appendChild(buttonGira)
 const dado = document.createElement("div")
 dado.classList.add("dado")
 
-
 document.body.appendChild(dado)
-
 
 let pegarResultado = [
                     0, 0, 0,
@@ -60,19 +45,6 @@ let pegarResultado = [
                     0, 0, 0, 
                     0, 0
                 ]
-
-let qtd2 = 0
-let qtd3 = 0
-let qtd4 = 0
-let qtd5 = 0
-let qtd6 = 0
-let qtd7 = 0
-let qtd8 = 0
-let qtd9 = 0
-let qtd10 = 0
-let qtd11= 0
-let qtd12= 0
-
 
 const buttonGiraMil = document.createElement("button")
     buttonGiraMil.classList.add("giraDados");
@@ -82,92 +54,100 @@ const buttonGiraMil = document.createElement("button")
     document.body.appendChild(buttonGiraMil)  
 
     buttonGiraMil.addEventListener("click", () => {
+
         for(let i = 0; i<1000; i++){
             
             let resultMil = girarDadosMil()
             
-            // let resultado = girarDadosMil()
-            
             if(resultMil === 2){
-                qtd2++
-                console.log("Deu o dois " + qtd2)
-                pegarResultado[0] = qtd2
+                pegarResultado[0]++
                 
             }
             if(resultMil === 3){
-                qtd3++
-                console.log("Deu o tres " + qtd3)
-                pegarResultado[1] = qtd3
+                pegarResultado[1]++
                 
             }
             if(resultMil === 4){
-                qtd4++
-                pegarResultado[2] = qtd4
+                pegarResultado[2]++
                 
             }
             if(resultMil === 5){
-                qtd5++
-                pegarResultado[3] = qtd5
+                pegarResultado[3]++
                 
             }
             if(resultMil === 6){
-                qtd6++
-                pegarResultado[4] = qtd6
+                pegarResultado[4]+=1
                 
             }
             if(resultMil === 7){
-                qtd7++
-                pegarResultado[5] = qtd7
+                pegarResultado[5]+=1
                 
             }
             if(resultMil === 8){
-                qtd8++
-                pegarResultado[6] = qtd8
+                pegarResultado[6]+=1
                 
             }
             if(resultMil === 9){
-                qtd9++
-                pegarResultado[7] = qtd9
+                pegarResultado[7]+=1
                 
             }
             if(resultMil === 10){
-                qtd10++
-                pegarResultado[8] = qtd10
+                pegarResultado[8]+=1
                 
             }
             if(resultMil === 11){
-                qtd11++
-                pegarResultado[9] = qtd11
+                pegarResultado[9]+=1
                 
             }
             if(resultMil === 12){
-                qtd12++
-                pegarResultado[10] = qtd12
+                pegarResultado[10]+=1
                 
             }
         
         }  
 
+        console.log(pegarResultado[0])
 
-        for(let i = 0; i < 11; i++){
-            
+        const main = document.createElement("main") 
+                main.classList.add("main")
+                document.body.appendChild(main)
+
+        
+    
+            const graficoTexto = document.createElement("div") 
+            graficoTexto.classList.add("graficoTexto")
+            main.appendChild(graficoTexto)  
+            for(let i = 0; i < 11; i++){
+                const graficoTextoH6 = document.createElement("h6") 
+                graficoTextoH6.classList.add("graficoTextoH6")
+                graficoTexto.appendChild(graficoTextoH6)  
+                graficoTextoH6.innerText = ("Deu o N° " + (i+2) + ": " + pegarResultado[i] + " vezes")
+            }
+
+            const text = document.createElement("h2") 
+            text.classList.add("text")
+            main.appendChild(text)
+            text.innerText = ("GRÁFICO")
+
             const grafico = document.createElement("div") 
             grafico.classList.add("grafico")
-            main.appendChild(grafico)        
-            grafico.style.width = pegarResultado[i]*5 + "px"
+            main.appendChild(grafico)  
             
-            grafico.innerText = ("deu o " + (i+2) + " - " + pegarResultado[i] + " vezes")
+            for(let i = 0; i < 11; i++){
+            const graficoContent = document.createElement("div") 
+            graficoContent.classList.add("graficoContent")
+            grafico.appendChild(graficoContent)       
+            graficoContent.style.height = pegarResultado[i]*1 + "px"
+           
 
             } 
+        
             
      })
 
      
         
-        const main = document.createElement("main") 
-                main.classList.add("main")
-                document.body.appendChild(main)
-                
+        
                 
     
     
